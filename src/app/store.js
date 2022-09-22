a8b72fb19876465a581017da7d4629d1cd8121e8
EliedersonLinhares <eliederson250@gmail.com>
2022-09-22 12:17:48 -0300

    first commit2

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postReducer from '../feature/posts/postsSlice'
+import usersReducer from '../feature/users/usersSlice'
+
+export const store = configureStore({
+	reducer: {
+		posts: postReducer,
+		users: usersReducer,
+	},
+})
